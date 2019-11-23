3f0db0865da1e4c8eb4957fccb9d3681a49f8bd1
kkubesh <kube0057@umn.edu>
2019-11-22 20:46:03 -0600

    remove emotion

--- src/components/OneYear.tsx
+++ src/components/OneYear.tsx
@@ -2,11 +2,6 @@ import * as React from "react";
 import { Card } from "@material-ui/core";
 import '../App.css';
 import { JobsData } from "../types/job";
-// import { jsx, css } from "@emotion";
-
-const someStyle = css`
-  background-color: blue;
-`
 
 type OneYearProps = {
     year: string;
@@ -17,7 +12,7 @@ export const OneYear: React.FunctionComponent<OneYearProps> = ({year, jobs}) => 
 
   return (
     <Card className="OneYear">
-        <h3 css={someStyle}>{year}</h3>
+        <h3>{year}</h3>
         <p>Title: {jobs[0].job_title}</p>
         <p>Title: {jobs[1].job_title}</p>
     </Card>
